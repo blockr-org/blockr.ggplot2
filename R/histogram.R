eb3bef44945f7c0a772a730547ed612c37e27c38
Dhadage <ramnathbalasaheb.dhadage@bms.com>
2024-02-23 18:12:08 +0530

    created histogram block and register it

---
+++ R/histogram.R
@@ -0,0 +1,25 @@
+#' Histogram block
+#' 
+#' Add histogram to a ggplot2 object.
+#' 
+#' @param data Data to be plotted.
+#' @param ... Passed to [blockr::new_block()].
+#' 
+#' @importFrom blockr new_block
+#' @importFrom ggplot2 geom_histogram
+#' 
+#' @export
+geomhistogram_block <- function(data, ...) {
+  initialize_block(new_geomhistogram_block(data, ...), data)
+}
+
+new_geomhistogram_block <- function(data, ...) {
+  new_block(
+    fields = list(),
+    expr = quote(
+      geom_histogram()
+    ),
+    class = c("geomhistogram_block", "plot_layer_block", "plot_block"),
+    ...
+  )
+}
